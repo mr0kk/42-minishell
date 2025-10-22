4a28e9a78646ead5a93a8c63e066bbdaf9dd1503
Radoslaw Mrok <rmrok@c3r16s3.42warsaw.pl>
2025-10-22 23:04:43 +0200

    parsing started

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,16 +6,24 @@
 /*   By: rmrok <rmrok@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 20:47:29 by rmrok             #+#    #+#             */
-/*   Updated: 2025/10/22 20:57:22 by rmrok            ###   ########.fr       */
+/*   Updated: 2025/10/22 21:55:14 by rmrok            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	free_table(char **tab)
+{
+	int	i;
+
+	i = 0; 
+}
 
 void	parser(t_data *data)
 {
 	char	**instructions;
 
 	printf("%s\n", data->user_input);
-	instructions = ft_split(data->user_input);
+	instructions = ft_split(data->user_input, ' ');
+	
 }
